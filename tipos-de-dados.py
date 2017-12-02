be89c149add5ca7c2c72a188a3bf5251ae075661
anderson-matheus <matheus5347@gmail.com>
2017-12-02 15:24:48 -0200

    Helo world, utilizando variáveis e tipos de dados

---
+++ tipos-de-dados.py
@@ -0,0 +1,8 @@
+idade = 21
+print(type(idade))
+
+pi = 3.14
+print(type(pi))
+
+nome = 'Marcos'
+print(type(nome))
---
+++ utilizando-variaveis.py
@@ -0,0 +1,12 @@
+nome = 'Anderson Matheus'
+print(nome)
+
+idade = 21
+print(idade)
+
+pi = 3.141592
+print(pi)
+
+print(type(nome))
+print(type(idade))
+print(type(pi))
